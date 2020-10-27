1e02e99fe255181299544ab58148d32c9ecd9266
Dhruv Peter <dhruveldhopeter@gmail.com>
2020-10-27 20:18:56 +0530

    Updates

---
+++ precache-manifest.54e0d719d1ea3f6cbf862f52914fb0db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ae0479e132d67cc0a987da205e7b9c",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "537896a2a9815969a4d6",
+    "url": "/TodoApp/static/css/main.6522ecd8.chunk.css"
+  },
+  {
+    "revision": "c0ac3323f3c7070c9b0b",
+    "url": "/TodoApp/static/js/2.1eb36aa6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/TodoApp/static/js/2.1eb36aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537896a2a9815969a4d6",
+    "url": "/TodoApp/static/js/main.3eec086b.chunk.js"
+  },
+  {
+    "revision": "6178001199e45a76af3f",
+    "url": "/TodoApp/static/js/runtime-main.579f7b68.js"
+  }
+]);
